0732976e6c8d153ae923edf28058b029a34e8c56
PROGRAMACION1 <PROGRAMACION1@clinicapalmira.local>
2022-09-01 16:05:37 -0500

    1.0

---
+++ d.js
@@ -0,0 +1,53 @@
+const CASAS = 
+[
+    {
+        color: "amarilla",
+        bebida: "cerveza",
+        mascota: "",
+        deporte: "cricket",
+        nacionalidad: "nigeriano"
+    },
+    {
+        color: "azul",
+        bebida: "té",
+        mascota: "caballo",
+        deporte: "",
+        nacionalidad: "irlandes"
+    },
+    {
+        color: "marfil",
+        bebida: "leche",
+        mascota: "",
+        deporte: "",
+        nacionalidad: ""
+    },
+    {
+        color: "verde",
+        bebida: "cafe",
+        mascota: "",
+        deporte: "",
+        nacionalidad: ""
+    },
+    {
+        color: "roja",
+        bebida: "jugo de naranja",
+        mascota: "",
+        deporte: "poker",
+        nacionalidad: "ingles"
+    }
+];
+
+//Mascotas
+cebra
+perro
+lobo
+caracol
+
+//Deportes
+GO   -  caracol
+Polo 
+Judo
+
+//Nacionalidad
+Español - perro
+Japones - Polo
